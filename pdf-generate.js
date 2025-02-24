ad0c5ad62603cdb65e7eef4f1a736235d9a1a611
Cedrick Campoto <cedrickcampoto@yahoo.com>
2025-02-24 09:08:14 +0800

    update

--- pdf-generate.js
+++ pdf-generate.js
@@ -1,23 +1,17 @@
 require('dotenv').config();
 const puppeteer = require("puppeteer");
 
-let browser;
-
 module.exports = async (html)=>{
-    if(!browser){
-        browser = await puppeteer.launch({
-            headless: true,
-            executablePath: process.env.CHROME_BIN,
-            args: ["--no-sandbox", "--disabled-setupid-sandbox"],
-        });
-    }
+    const browser = await puppeteer.launch({
+        headless: true,
+        executablePath: process.env.CHROME_BIN,
+        args: ["--no-sandbox", "--disabled-setupid-sandbox"],
+    });
     const page = await browser.newPage();
     await page.setContent(html);
     const pdf = await page.pdf({ format: "A4" });
     await page.close();
 
-
-    console.log(16,html);
-
+    console.log(15,pdf);
     return pdf;
 }
